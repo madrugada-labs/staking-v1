570e4837f18601e3aa31e98ca018a8ea501ba1a3
Dhruv D Jain <dhruv@iamsizzling.com>
2022-05-25 11:25:39 +0530

    created initialize function for job factory

--- programs/job_factory/src/lib.rs
+++ programs/job_factory/src/lib.rs
@@ -6,10 +6,31 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod job_factory {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, job_ad_id: String, max_amount_per_application: u32) -> Result<()> {
+
+        let parameters = &mut ctx.accounts.base_account;
+
+        parameters.authority = ctx.accounts.authority.key();
+        parameters.job_ad_id = job_ad_id;
+        parameters.max_amount_per_application = max_amount_per_application;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(job_ad_id: String)]
+pub struct Initialize<'info> {
+    #[account(init, payer = authority, seeds = [b"job_factory", job_ad_id.as_bytes()[..18].as_ref(), job_ad_id.as_bytes()[18..].as_ref()], bump, space = 4 + 32 + 40 )]
+    pub base_account: Account<'info, JobStakingParameter>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>                                                 
+}
+
+#[account]
+pub struct JobStakingParameter {
+    pub authority: Pubkey, // 32 bytes
+    pub job_ad_id: String, // 40 bytes
+    pub max_amount_per_application: u32 // 4 bytes
+}
