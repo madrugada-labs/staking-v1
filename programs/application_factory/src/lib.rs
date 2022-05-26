0d6f1708cf932cf04486a1420ce686c0a7ef8788
Dhruv D Jain <dhruv@iamsizzling.com>
2022-05-26 13:07:21 +0530

    created application factory and with initialize and update status function

--- programs/application_factory/src/lib.rs
+++ programs/application_factory/src/lib.rs
@@ -2,14 +2,48 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const JOB_APPLICATION_SEED: &'static [u8] = b"job_application";
+
 #[program]
 pub mod application_factory {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, job_ad_id: String) -> Result<()> {
+
+        let details = &mut ctx.accounts.base_account;
+
+        details.status = false,
+        details.stake_amount = 0;
+        details.job_ad_id = job_ad_id;
+        details.authority = ctx.accounts.authority.key();
+
+        Ok(())
+    }
+
+    pub fn update_status(ctx: Context<UpdateStatus>, job_status: bool) -> Result<()> {
+
+        let details = &mut ctx.accounts.base_account;
+
+        details.status = job_status;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(job_ad_id: String)]
+pub struct Initialize<'info> {
+    #[account(init, payer = authority, seeds = [JOB_APPLICATION_SEED, job_ad_id.as_bytes()[..18].as_ref(), job_ad_id.as_bytes()[18..].as_ref(), authority.key().as_ref()], bump, space = )]
+    pub base_account: Account<'info, JobApplication>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct JobApplication {
+    pub status: bool,
+    pub stake_amount: u32,
+    pub job_ad_id: String,
+    pub authority: Pubkey
+}
